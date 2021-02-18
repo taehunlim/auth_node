b9f00be2cffb9e6e2d5cd2fa33a708b559f8fa55
limtae <dkslsdkdl@naver.com>
2021-02-18 21:13:30 +0900

    Redux init ( store, rootReducers )

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,29 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Home from "./screens/Home";
 import Writing from "./screens/Writing";
 
-import "./assets/scss/styles.scss"
+import "./assets/scss/styles.scss";
 import 'react-toastify/dist/ReactToastify.css';
 
+import {Provider} from 'react-redux';
+import store from './store';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+
+const persist = persistStore(store)
 
 function App() {
   return (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact render={ props => <Home {...props} /> } />
-            <Route path="/write" render={ props => <Writing {...props} /> } />
-            <Redirect to="/" />
-        </Switch>
-    </BrowserRouter>
+      <Provider store={store}>
+          <PersistGate loading={null} persistor={persist}>
+              <BrowserRouter>
+                  <Switch>
+                      <Route path="/" exact render={ props => <Home {...props} /> } />
+                      <Route path="/write" render={ props => <Writing {...props} /> } />
+                      <Redirect to="/" />
+                  </Switch>
+              </BrowserRouter>
+          </PersistGate>
+      </Provider>
   );
 }
 
